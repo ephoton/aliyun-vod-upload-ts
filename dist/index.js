b59fec16d2f5e66a7accf753551f3398789c8b41
ephoton <i@ephoton.me>
2020-04-11 07:23:39 +0800

    feat: updated

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+export var AliyunUpload = require('./lib/aliyun-upload-sdk-1.5.0.min');
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,1 @@
+export const AliyunUpload: any = require('./lib/aliyun-upload-sdk-1.5.0.min');
